7c6a4030957e58ee49bf398fb0881d299d9cf090
Moon Antonio <pinchodev91@gmail.com>
2018-03-01 21:29:57 +0100

    Add inspector script for CamaraControl.cs

---
+++ UCamaraSetup Project/Assets/Proyecto UCamaraSetup/Scripts/Editor/CamaraControllerInspector.cs
@@ -0,0 +1,25 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// CamaraControllerInspector.cs (01/03/2018)									\\
+// Autor: Antonio Mateo (Moon Antonio) 									        \\
+// Descripcion:		Inspector de CamaraController								\\
+// Fecha Mod:		01/03/2018													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+using UnityEditor;
+#endregion
+
+namespace MoonAntonio
+{
+	/// <summary>
+	/// <para>Inspector de CamaraController.</para>
+	/// </summary>
+	[CustomEditor(typeof(CamaraController))]
+	public class CamaraControllerInspector : Editor
+	{
+
+	}
+}

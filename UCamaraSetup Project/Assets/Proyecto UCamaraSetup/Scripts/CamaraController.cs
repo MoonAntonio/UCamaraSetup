f9f65ffad8dee06462a8ad6cbe6ee32e4141f83c
Moon Antonio <pinchodev91@gmail.com>
2018-03-01 19:35:15 +0100

    Variables activadoras

--- UCamaraSetup Project/Assets/Proyecto UCamaraSetup/Scripts/CamaraController.cs
+++ UCamaraSetup Project/Assets/Proyecto UCamaraSetup/Scripts/CamaraController.cs
@@ -1,7 +1,31 @@
-﻿using UnityEngine;
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// CamaraController.cs (01/03/2018)												\\
+// Autor: Antonio Mateo (Moon Antonio) 									        \\
+// Descripcion:		Control de la camara										\\
+// Fecha Mod:		01/03/2018													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
 
-public class CamaraController : MonoBehaviour
+#region Librerias
+using UnityEngine;
+#endregion
+
+
+namespace MoonAntonio
 {
+	/// <summary>
+	/// <para>Control de la camara.</para>
+	/// </summary>
+	public class CamaraController : MonoBehaviour
+	{
+		#region Variables Activacion
+		public bool horizontal = false;
+		public bool vertical = false;
+		public bool verticalGlobal = false;
+		public bool zoom = false;
+		public bool autorotacion = false;
+		#endregion
 
-
+	}
 }
